254b0778f278a52fa79b77a4747ce2e2708ac4f0
dbtest <dbtest@aa>
2019-11-20 05:54:29 +0900

    1120 /kakao 부분수정 국푸름

--- finalProject/src/main/java/com/kh/letEatGo/member/controller/MemberController.java
+++ finalProject/src/main/java/com/kh/letEatGo/member/controller/MemberController.java
@@ -104,62 +104,62 @@ public class MemberController {
 
 	  }
 	  @RequestMapping("/kakao")
-	  public ModelAndView login(String email,String gender,String id, HttpSession session) {
-		  ModelAndView mv=new ModelAndView();
-		  Member m=new Member();
-		  String msg="dd";
-		  String loc="";
-		  m.setMember_Id(id);
-		  m.setMember_Password("비밀번호를 입력하시오.");
-		  m.setMember_Age("나이를 입력하시오.");
-		  m.setMember_Phone("전화번호를 입력하시오.");
-		  if(gender!=null) {
-			  if(gender==("male")) {
-			  m.setMember_Gender("남");
-			  }else {
-			  m.setMember_Gender("여");
-			  }
-		  }else {
-		  m.setMember_Gender("성별을 입력하시오.");
-		  }
-		  if(email!=null) {
-			  m.setMember_Email(email);
-		  }else {
-			  m.setMember_Email("이메일을 입력하시오.");
-		  }
-		  
-		  Member result2;
-		  
-		  result2 = service.selectMemberOne(m);
-		  mv.setViewName("redirect:/");
-		  if(result2==null) {
-			  int result=service.insertKakao(m);
-			  result2 = service.selectMemberOne(m);
-			  mv.setViewName("/mypage/memberPage");
-		  }
-		  try {
-			result2.setMember_Phone(enc.decrypt(result2.getMember_Phone()));
-			result2.setMember_Email(enc.decrypt(result2.getMember_Email()));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 
-		  session.setAttribute("loginMember", result2);
-		  session.setAttribute("type", "member");
-		  Member result3=service.selectMemberOne(m);
-		  session.setAttribute("member",result3);
-		  try {
-			result3.setMember_Phone(enc.decrypt(result3.getMember_Phone()));
-			result3.setMember_Email(enc.decrypt(result3.getMember_Email()));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		  //mv.setViewName("redirect:/");
-		return mv;
+	     public ModelAndView login(String email,String gender,String id, HttpSession session) {
+	        ModelAndView mv=new ModelAndView();
+	        Member m=new Member();
+	        String msg="dd";
+	        String loc="";
+	        m.setMember_Id(id);
+	        m.setMember_Password("비밀번호를 입력하시오.");
+	        m.setMember_Age("20");
+	        m.setMember_Phone("전화번호를 입력하시오.");
+	        if(gender!=null) {
+	           if(gender.equals("male")) {
+	           m.setMember_Gender("남");
+	           }else {
+	           m.setMember_Gender("여");
+	           }
+	        }else {
+	           m.setMember_Gender("남");
+	        }
+	        if(email!=null) {
+	           m.setMember_Email(email);
+	        }else {
+	           m.setMember_Email("이메일을 입력하시오.");
+	        }
+	        
+	        Member result2;
+	        
+	        result2 = service.selectMemberOne(m);
+	        mv.setViewName("redirect:/");
+	        if(result2==null) {
+	           int result=service.insertKakao(m);
+	           result2 = service.selectMemberOne(m);
+	           mv.setViewName("/mypage/memberPage");
+	        }
+	        try {
+	         result2.setMember_Phone(enc.decrypt(result2.getMember_Phone()));
+	         result2.setMember_Email(enc.decrypt(result2.getMember_Email()));
+	      } catch (Exception e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      }
+	       
+	        session.setAttribute("loginMember", result2);
+	        session.setAttribute("type", "member");
+	        Member result3=service.selectMemberOne(m);
+	        session.setAttribute("member",result3);
+	        try {
+	         result3.setMember_Phone(enc.decrypt(result3.getMember_Phone()));
+	         result3.setMember_Email(enc.decrypt(result3.getMember_Email()));
+	      } catch (Exception e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      }
+	        //mv.setViewName("redirect:/");
+	      return mv;
 
-	  }
+	     }
 	  
 	  @RequestMapping("/Logout.do")
 		public String logout(HttpSession session,SessionStatus s) {
